7353543b854bcb3f5f5a641e3390780c076fde77
Erick Santos <erick.kine2012@hotmail.com>
2025-06-13 19:28:02 +0100

    desafio prioridades final

--- Desafio/Prioridade.py
+++ Desafio/Prioridade.py
@@ -10,12 +10,6 @@ for _ in range(n):
     idade = int(idade)
     pacientes.append((nome, idade, status))
 
-def exibir(data_extenso, *args, **kwargs):
-    texto = "\n".join(args)
-    meta_dados = "\n".join([f"{chave.title()}: {valor}" for chave, valor in kwargs.items()])
-    mensagem = f"{data_extenso}\n\n{texto}\n\n{meta_dados}"
-    print(mensagem)
-
 class Paciente:
     def __init__(self, nome, idade, status):
         self.nome = nome
@@ -35,19 +29,13 @@ class Paciente:
 # Ordenação por prioridade e nome
 ordem_prioridade = sorted(
     pacientes,
-    key=lambda p: (-Paciente(*p).prioridade(), p[0])
+    key=lambda p: (-Paciente(*p).prioridade(), -p[1], p[2])
 )
-
 # Formatação dos pacientes
 paciente_prioridade = [f"{nome}, {idade}, {status}" for nome, idade, status in ordem_prioridade]
 nomes_prioridade = [nome for nome, _, _ in ordem_prioridade]
 
 # Exibição final
-exibir(
-    "Ordem de atendimento:",
-    *paciente_prioridade,
-    Total=len(pacientes)
-    )
-#list_pacient("Zen of Python", "Beautiful is better than ugly.", autor="Tim Peters", ano=1999)
+print("Ordem de Atendimento:", ", ".join(nomes_prioridade))
 
 
